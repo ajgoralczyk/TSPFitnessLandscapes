a4ae963d2baf3b631325f34fed2d3301d33a67b8
Agnieszka Góralczyk <a.j.goralczyk@gmail.com>
2019-03-12 15:57:52 +0100

    broken indexes

--- new/lin_kernighan.py
+++ new/lin_kernighan.py
@@ -1,4 +1,5 @@
 from helpers import *
+from tour_check import *
 from itertools import starmap
 
 class LK:
@@ -8,15 +9,15 @@ class LK:
         self.closest_nodes = closest_nodes
 
     def closest_tuples(self):
-        def closest_tuple(node1, node2, indexes):
-            return (node2, indexes[node2], self.A[node1, node2])
+        def closest_tuple(node1, node2):
+            return (node2, self.indexes[node2], self.A[node1, node2])
 
         closest = {}
-        indexes = {}
+        self.indexes = {}
         for i in range(self.n):
-            indexes[self.tour[i]] = i
+            self.indexes[self.tour[i]] = i
         for i in range(self.n):
-            closest[i] = list(starmap(closest_tuple, [(i, node, indexes) for node in self.closest_nodes[i]]))
+            closest[i] = list(starmap(closest_tuple, [(i, node) for node in self.closest_nodes[i]]))
         return closest
 
     def optimize_path(self, tour): # Step 1
@@ -65,13 +66,14 @@ class LK:
 
             for t2, index_t2 in around_t1:  # Step 3, 11
                 broken = {(t1, t2), (t2, t1)}
+                broken_indexes = [min(index_t1, index_t2)]
                 gain = self.A[t1, t2]
                 closest_neighbours = self.closest(t2, gain, broken, closest_tuples[t2])
 
                 for t3, (_, Gi, index_t3) in closest_neighbours:
                     joined = {(t3, t2), (t2, t3)}  # Rule 1 - only sequential exchanges
 
-                    is_better, better_path, better_path_cost = self.chooseX(solutions, intersection, closest_tuples, t1, index_t1, t3, index_t3, Gi, broken, joined, 2)
+                    is_better, better_path, better_path_cost = self.chooseX(solutions, intersection, closest_tuples, t1, index_t1, t3, index_t3, Gi, broken, broken_indexes, joined, 2)
                     if is_better:
                         return is_better, better_path, better_path_cost
 
@@ -97,20 +99,20 @@ class LK:
 
         return sorted(neighbours.items(), key=lambda x: x[1][0], reverse=True)
 
-    def chooseX(self, solutions, intersection, closest_tuples, t1, index_t1, last, index_last, gain, broken, joined, depth):
-        # if len(broken) >= 9: ############################################# 1
+    def chooseX(self, solutions, intersection, closest_tuples, t1, index_t1, last, index_last, gain, broken, broken_indexes, joined, depth):
+        # if len(broken) >= 9: ##################################################### 1
         #     return False, None, None
         around = []
         new_tour = None
         if depth == 4:  # Rule 9 - selecting x4, larger c(x4)
             (pred, index_pred), (succ, index_succ) = nodes_around(self.tour, index_last)
 
-            tc1 = TourCheck(self.tour, broken | {(pred, last), (last, pred)}, joined | {(t1, pred), (pred, t1)})
+            tc1 = TourCheck(self.tour, broken | {(pred, last), (last, pred)}, broken_indexes + [min(index_last, index_pred)], joined | {(t1, pred), (pred, t1)})
             is_tour, new_tour = tc1.generate_tour()
             if pred != t1 and self.A[pred, last] > self.A[succ, last] and is_tour:
                 around = [(pred, index_pred)]
             else:
-                tc2 = TourCheck(self.tour, broken | {(succ, last), (last, succ)}, joined | {(t1, succ), (succ, t1)})
+                tc2 = TourCheck(self.tour, broken | {(succ, last), (last, succ)}, broken_indexes + [min(index_last, index_succ)], joined | {(t1, succ), (succ, t1)})
                 is_tour, new_tour = tc2.generate_tour()
                 if succ != t1 and is_tour:
                     around = [(succ, index_succ)]
@@ -127,7 +129,7 @@ class LK:
                 total_gain = new_gain - self.A[t2i, t1]
 
                 if new_tour is None:
-                    tc = TourCheck(self.tour, broken | {(last, t2i),(t2i, last)}, joined | {(t1, t2i), (t2i, t1)})
+                    tc = TourCheck(self.tour, broken | {(last, t2i),(t2i, last)}, broken_indexes + [min(index_t2i, index_last)], joined | {(t1, t2i), (t2i, t1)})
                     is_tour, new_tour = tc.generate_tour() # Rule 3 - selecting xi, (t2i, t1) must make a valid tour
                 else:
                     is_tour = True
@@ -139,7 +141,7 @@ class LK:
                     if total_gain > 0:
                         return True, new_tour, path_length(new_tour, self.A, self.n)
 
-                    choice, new_tour, new_tour_cost = self.chooseY(solutions, intersection, closest_tuples, t1, index_t1, t2i, new_gain, broken | {(last, t2i),(t2i, last)}, joined, depth)
+                    choice, new_tour, new_tour_cost = self.chooseY(solutions, intersection, closest_tuples, t1, index_t1, t2i, new_gain, broken | {(last, t2i),(t2i, last)}, broken_indexes + [min(index_t2i, index_last)], joined, depth)
                     if depth == 2 and not choice:
                         pass
                     else:
@@ -147,10 +149,10 @@ class LK:
 
         return False, None, None
 
-    def chooseY(self, solutions, intersection, closest_tuples, t1, index_t1, t2i, gain, broken, joined, depth):
+    def chooseY(self, solutions, intersection, closest_tuples, t1, index_t1, t2i, gain, broken, broken_indexes, joined, depth):
         closest_neighbours = self.closest(t2i, gain, broken, closest_tuples[t2i])
         for node, (_, Gi, index_node) in closest_neighbours:
-            is_better, new_tour, new_tour_cost = self.chooseX(solutions, intersection, closest_tuples, t1, index_t1, node, index_node, Gi, broken, joined | {(t2i, node), (node, t2i)}, depth+1)
+            is_better, new_tour, new_tour_cost = self.chooseX(solutions, intersection, closest_tuples, t1, index_t1, node, index_node, Gi, broken, broken_indexes, joined | {(t2i, node), (node, t2i)}, depth+1)
             if depth == 2 and not is_better:
                 pass
             else:
